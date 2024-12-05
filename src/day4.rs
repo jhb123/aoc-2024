31170b1b3ecce30c55ceedb5f08670505e984ec4
jhb123 <jhbriggs23@gmail.com>
2024-12-05 23:50:45 +0000

    solve part2 day 4

--- src/day4.rs
+++ src/day4.rs
@@ -8,6 +8,8 @@ struct WordGrid {
     width: usize,
     bytedata: Vec<u8>,
     target: Vec<u8>,
+    target2: Vec<u8>,
+    target3: Vec<u8>,
 }
 
 impl WordGrid {
@@ -16,11 +18,16 @@ impl WordGrid {
         let width = data.chars().take_while(|&c| c != '\n').count() + 1;
         let bytedata = data.clone().into_bytes();
         let target = String::from("XMAS").into_bytes();
+        let target2 = String::from("MSAMS").into_bytes();
+        let target3 = String::from("MMASS").into_bytes();
+
         Self {
             data,
             width,
             bytedata,
             target,
+            target2,
+            target3,
         }
     }
 
@@ -49,6 +56,25 @@ impl WordGrid {
                 count += 1
             }
             if self.check_diagonal_2b(i) {
+                count += 1
+            }
+        }
+        return count;
+    }
+
+    fn find_all2(self) -> usize {
+        let mut count = 0;
+        for i in 0..self.data.len() {
+            if self.check_x(i) {
+                count += 1
+            }
+            if self.check_xr(i) {
+                count += 1
+            }
+            if self.check_x1(i) {
+                count += 1
+            }
+            if self.check_xr1(i) {
                 count += 1
             }
         }
@@ -162,11 +188,68 @@ impl WordGrid {
 
         target.eq(test)
     }
+
+    fn check_x(&self, idx: usize) -> bool {
+        let offsets = [0, 2, self.width + 1, 2 * (self.width), 2 * (self.width) + 2];
+
+        offsets
+            .iter()
+            .enumerate()
+            .take_while(|(jdx, &offset)| self.bytedata.get(idx + offset) == self.target2.get(*jdx))
+            .count()
+            == 5
+    }
+
+    fn check_xr(&self, idx: usize) -> bool {
+        let offsets = [0, 2, self.width + 1, 2 * (self.width), 2 * (self.width) + 2];
+
+        offsets
+            .iter()
+            .enumerate()
+            .take_while(|(jdx, &offset)| {
+                if let Some(k) = self.bytedata.len().checked_sub(idx + offset) {
+                    self.bytedata.get(k) == self.target2.get(*jdx)
+                } else {
+                    false
+                }
+            })
+            .count()
+            == 5
+    }
+
+    fn check_x1(&self, idx: usize) -> bool {
+        let offsets = [0, 2, self.width + 1, 2 * (self.width), 2 * (self.width) + 2];
+
+        offsets
+            .iter()
+            .enumerate()
+            .take_while(|(jdx, &offset)| self.bytedata.get(idx + offset) == self.target3.get(*jdx))
+            .count()
+            == 5
+    }
+
+    fn check_xr1(&self, idx: usize) -> bool {
+        let offsets = [0, 2, self.width + 1, 2 * (self.width), 2 * (self.width) + 2];
+
+        offsets
+            .iter()
+            .enumerate()
+            .take_while(|(jdx, &offset)| {
+                if let Some(k) = self.bytedata.len().checked_sub(idx + offset) {
+                    self.bytedata.get(k) == self.target3.get(*jdx)
+                } else {
+                    false
+                }
+            })
+            .count()
+            == 5
+    }
 }
 
 pub fn solution() {
     let grid = WordGrid::new("day4.txt");
-    println!("day 4 part 1: {}", grid.find_all())
+    println!("day 4 part 1: {}", grid.find_all());
+    println!("day 4 part 1: {}", grid.find_all2());
 }
 
 #[cfg(test)]
@@ -195,5 +278,17 @@ mod tests {
     fn test_check_xmas4() {
         let grid = WordGrid::new("day4.txt");
         assert_eq!(2297, grid.find_all())
+    }
+
+    // #[test]
+    // fn test_check_xmas5() {
+    //     let grid = WordGrid::new("day4test.txt");
+    //     assert_eq!(9, grid.find_all2())
+    // }
+
+    #[test]
+    fn test_check_xmas6() {
+        let grid = WordGrid::new("day4test2.txt");
+        assert_eq!(1, grid.find_all2())
     }
 }
